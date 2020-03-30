1c4117f62c2a323a6112e165658cba37bd209c59
Francesco <francesco.guidi@yourbiz.it>
2020-03-30 11:52:25 +0200

    first commit

---
+++ editor.controller.js
@@ -0,0 +1,36 @@
+﻿angular.module("umbraco").controller("yb.gridyoutubeeditor", function ($scope, editorService) {
+
+    var vm = this;
+    vm.OpenEditor = openEditor;
+
+    $scope.title = "Youtube video options";
+    
+    function openEditor() {
+
+        //set custom options
+        var options = {
+            title: "Youtube video options",
+            size: "small",
+            editorData: $scope.control.value,
+            view: "/App_Plugins/GridYoutubeEditor/youtubevideodetail.html",
+            submit: function (value) {
+                
+                //pass value to model if not empty
+                if (value != null && value.videourl !== '') {
+                    $scope.control.value = {};
+                    $scope.control.value.videourl = value.videourl;
+                    $scope.control.value.allowfullscreen = value.allowfullscreen;
+                }
+
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+
+        //open editor with custom options
+        editorService.open(options);
+    };
+
+});
---
+++ infiniteeditor.controller.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    "use strict";
+
+    function InfiniteEditorController($scope) {
+
+        if ($scope.model.editorData != undefined) {
+            //passing values to model
+            $scope.model.videourl = $scope.model.editorData.videourl;
+            $scope.model.allowfullscreen = $scope.model.editorData.allowfullscreen;
+        }
+
+        var vm = this;
+
+        vm.submit = submit;
+        vm.close = close;
+
+        function submit() {
+            if ($scope.model.submit) {
+                $scope.model.submit($scope.model);
+            }
+        }
+
+        function close() {
+            if ($scope.model.close) {
+                $scope.model.close();
+            }
+        }
+
+    }
+
+    angular.module("umbraco").controller("yb.gridyoutubeinfiniteeditor", InfiniteEditorController);
+})();
